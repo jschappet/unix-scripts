045997e6ca91815735f73d9ccaa33a76d768b34f
Jimmy Schapept <jimmy@schappet.com>
2013-12-25 20:35:36 -0500

    add user input function

--- nice.py
+++ nice.py
@@ -1,5 +1,11 @@
 
+
+'''
+var = raw_input("Enter something: ")
+print "you entered ", var
+'''
+
 a={'John':'Naughty','Clara':'Nice'}
 for name, status in a.iteritems():
     if (status == 'Nice'):
-	print name + ": "+ status
+	print name , ": ", status
